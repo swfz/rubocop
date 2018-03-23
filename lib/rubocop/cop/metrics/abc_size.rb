192db5ddab3e5d452a547257641e5c3e94a4a7d5
swfz <sawafuji.09@gmail.com>
2018-03-24 02:39:11 +0900

    abc detail displayed

--- lib/rubocop/cop/metrics/abc_size.rb
+++ lib/rubocop/cop/metrics/abc_size.rb
@@ -10,11 +10,33 @@ module RuboCop
         include MethodComplexity
 
         MSG = 'Assignment Branch Condition size for %<method>s is too high. ' \
-              '[%<complexity>.4g/%<max>.4g]'.freeze
+          '[%<complexity>.4g/%<max>.4g] a: %<a>.4g, b: %<b>.4g, c: %<c>.4g'.freeze
         BRANCH_NODES = %i[send csend].freeze
         CONDITION_NODES = CyclomaticComplexity::COUNTED_NODES.freeze
 
         private
+
+        def check_complexity(node, method_name)
+          # Accepts empty methods always.
+          return unless node.body
+
+          max = cop_config['Max']
+          result = complexity(node.body)
+
+          return unless result[:complexity] > max
+
+          msg = format(self.class::MSG,
+                       method: method_name,
+                       complexity: result[:complexity],
+                       a: result[:a],
+                       b: result[:b],
+                       c: result[:c],
+                       max: max)
+
+          add_offense(node, message: msg) do
+            self.max = result[:complexity].ceil
+          end
+        end
 
         def complexity(node)
           assignment = 0
@@ -31,7 +53,12 @@ module RuboCop
             end
           end
 
-          Math.sqrt(assignment**2 + branch**2 + condition**2).round(2)
+          {
+            a: assignment,
+            b: branch,
+            c: condition,
+            complexity: Math.sqrt(assignment**2 + branch**2 + condition**2).round(2)
+          }
         end
       end
     end
